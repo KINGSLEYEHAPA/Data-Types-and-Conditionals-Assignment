a3d85e5ab08fb4c2b23207979643d99f869bbaa0
KINGSLEYEHAPA <kessity09@gmail.com>
2021-10-23 14:19:59 +0100

    Loop Assignment Update

--- loops-assignmnet.js
+++ loops-assignmnet.js
@@ -217,6 +217,161 @@ while(time>=0){
 
 }
 
+// 4. Rewrite the following while loop as a for loop:
+// var x = 9;
+// while (x >= 1) {
+//   console.log("hello " + x);
+//   x = x - 1;
+// }
+// Your Code:
+/*
+ * Programming Quiz
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `x` with the value of `9`
+ * - Your `for` loop should have a stop condition for the value of `x`
+ * - Your `for` loop should decrement `x` each time it executes
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+ 
+// rewrite the while loop as a for loop
+// var x = 9;
+// while (x >= 1) {
+//     console.log("hello " + x);
+//     x = x - 1;
+// }
+
+
+for(let x=9;x>=1; x--){
+
+    console.log("hello " + x);
+
+}
+
+
+
+// 5. Here is a for loop that's supposed to print the numbers 5 through 9. Fix the error!
+// for (x < 10; x++) {
+//   console.log(x);
+// }
+// Your Code:
+/*
+ * Programming Quiz: Fix the Error 
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `x` with the value of `5`
+ * - Your `for` loop should have a stop condition for the value of `x`
+ * - Your `for` loop should increment `x` each time it executes
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+// fix the for loop
+for (let x=5;x < 10; x++) {
+    console.log(x);
+}
+
+
+
+
+// 6. The for loop below has an error. Fix it!
+// for (let k = 0 k < 200 k++) {
+//   console.log(k);
+// }
+// Your Code:
+/*
+ * Programming Quiz: Fix the Error
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `k` with the value of `0`
+ * - Your `for` loop should have a stop condition for the value of `k`
+ * - Your `for` loop should increment `k` each time it executes
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+// fix the for loop
+for (let k = 0; k < 200;k++) {
+    console.log(k);
+}
+
+
+
+
+// 7. Write a for (note: not a function) loop that prints out the factorial of the number 12:
+// A factorial is calculated by multiplying a number by all the numbers below it. For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+// 3! = 3 * 2 * 1 = 6
+// 3!=3∗2∗1=6
+// 4! = 4 * 3 * 2 * 1 = 24
+// 4!=4∗3∗2∗1=24
+// 5! = 5 * 4 * 3 * 2 * 1 = 120
+// 5!=5∗4∗3∗2∗1=120
+// Save your final answer in a variable called solution and print it to the console.
+// Your Code:
+/*
+ * Programming Quiz: Factorials 
+ */
+
+// your code goes here
+var solution=1;
+for (var num =12; num>=1; num--){
+
+    
+    solution=solution*num;
+
+
+}
+console.log(solution);
+
+
+
+
+
+
+// 8. Theater seats often display a row and seat number to help theater goers find their seats. If there are 26 rows (0 to 25) and 100 seats (0 to 99) in each row, write a nested for loop to print out all of the different seat combinations in the theater.
+// Example output for row-seat information: output each row and seat number on a separate line
+// 0-0
+// 0-1
+// 0-2
+// ...
+// 25-97
+// 25-98
+// 25-99
+/*
+ * Programming Quiz: Find my Seat
+ * 
+ * Write a nested for loop to print out all of the different seat combinations in the theater.
+ * The first row-seat combination should be 0-0 
+ * The last row-seat combination will be 25-99
+ * 
+ * Things to note: 
+ *  - the row and seat numbers start at 0, not 1
+ *  - the highest seat number is 99, not 100
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a nested `for` loop'
+ * - Your code should produce the expected output, as explained above
+ */
+// Write your code here
+
+
+for (let row = 0; row < 26 ; row++) {
+    for (let seat = 0; seat < 100; seat++) {
+      console.log(`${row}-${seat}`);
+    }
+  }
+  
+
 
 
 
